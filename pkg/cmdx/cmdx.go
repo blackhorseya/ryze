7d0129e996ec71421f25a83ed25d52272df672df
Sean Zheng <blackhorseya@gmail.com>
2024-07-26 12:48:02 +0800

    feat: create interfaces and implementations for new services and commands
    - Add a new file `adapterx.go` in the `pkg/adapterx` directory
    - Define interfaces for `Service` and `Restful` in `adapterx.go`
    - Implement methods for `Service` and `Restful` interfaces in `adapterx.go`
    - Add a new file `cmdx.go` in the `pkg/cmdx` directory
    - Define a `ServiceCmd` struct in `cmdx.go`
    - Implement methods for creating a new service command in `cmdx.go`
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

---
+++ pkg/cmdx/cmdx.go
@@ -0,0 +1,48 @@
+package cmdx
+
+import (
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/blackhorseya/ryze/pkg/adapterx"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+// ServiceCmd represents the service command.
+type ServiceCmd struct {
+	Use        string
+	Short      string
+	GetService func(v *viper.Viper) (adapterx.Restful, error)
+}
+
+// NewServiceCmd creates a new service command.
+func NewServiceCmd(use string, short string, svc func(v *viper.Viper) (adapterx.Restful, error)) *cobra.Command {
+	return (&ServiceCmd{Use: use, Short: short, GetService: svc}).NewCmd()
+}
+
+// NewCmd creates a new service command.
+func (c *ServiceCmd) NewCmd() *cobra.Command {
+	return &cobra.Command{
+		Use:   c.Use,
+		Short: c.Short,
+		Run: func(cmd *cobra.Command, args []string) {
+			v := viper.New()
+
+			service, err := c.GetService(v)
+			cobra.CheckErr(err)
+
+			err = service.Start()
+			cobra.CheckErr(err)
+
+			signalChan := make(chan os.Signal, 1)
+			signal.Notify(signalChan, syscall.SIGTERM, syscall.SIGINT)
+
+			<-signalChan
+
+			err = service.AwaitSignal()
+			cobra.CheckErr(err)
+		},
+	}
+}
