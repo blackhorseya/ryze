886aae7bb21d94810029e1f84589006587057f77
Sean Zheng <blackhorseya@gmail.com>
2024-07-26 13:03:56 +0800

    feat: refactor codebase to improve code readability
    - Add a `fmt` import to the `stats.go` file
    - Add a `Run` function to print `stats` in the `statsCmd` command
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- cmd/get/stats.go
+++ cmd/get/stats.go
@@ -1,10 +1,15 @@
 package get
 
 import (
+	"fmt"
+
 	"github.com/spf13/cobra"
 )
 
 var statsCmd = &cobra.Command{
 	Use:   "stats",
 	Short: "Get stats",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("stats")
+	},
 }
