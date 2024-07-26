a2ab07a0c4573d93f6c8477d5a3651a99305b581
Sean Zheng <blackhorseya@gmail.com>
2024-07-26 12:47:19 +0800

    feat: consolidate error handling and API responses into new packages
    - Create a new package `errorx` with custom error type `Error`
    - Define methods `New` and `Wrap` for creating and wrapping errors
    - Create a new package `responsex` with response struct `Response`
    - Implement functions `OK`, `WithCode`, and `Err` for handling API responses
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

---
+++ pkg/responsex/responsex.go
@@ -0,0 +1,37 @@
+package responsex
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+// Response is a struct that represents the response of the API.
+type Response struct {
+	Code    int    `json:"code,omitempty"`
+	Message string `json:"message,omitempty"`
+	Data    any    `json:"data,omitempty"`
+}
+
+// OK is a function that returns a response with status code 200.
+func OK(c *gin.Context, data any) {
+	c.JSON(http.StatusOK, Response{
+		Code:    http.StatusOK,
+		Message: "ok",
+		Data:    data,
+	})
+}
+
+// WithCode is a function that returns a response with the specified status code.
+func WithCode(c *gin.Context, code int, data any) {
+	c.JSON(code, Response{
+		Code:    code,
+		Message: "ok",
+		Data:    data,
+	})
+}
+
+// Err is a function that returns a response with status code 500.
+func Err(c *gin.Context, err error) {
+	_ = c.Error(err)
+}
