9bf168e6a311ad41d8c2c50845b4dc5c9e02e371
Sean Zheng <blackhorseya@gmail.com>
2024-07-27 10:51:00 +0800

    feat: refactor network selection and scanning logic
    - Add viper and configx imports
    - Initialize startFlag and endFlag variables
    - Add config handling and network selection logic
    - Print scanning message with selected network
    - Handle start and end block numbers for scanning
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- cmd/scan.go
+++ cmd/scan.go
@@ -1,17 +1,57 @@
 package cmd
 
 import (
+	"github.com/blackhorseya/ryze/app/infra/configx"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var (
+	startFlag int
+	endFlag   int
 )
 
 var scanCmd = &cobra.Command{
 	Use:   "scan",
 	Short: "Scan the blockchain",
 	Run: func(cmd *cobra.Command, args []string) {
-		cmd.Println("scan called")
+		config, err := configx.NewConfiguration(viper.GetViper())
+		cobra.CheckErr(err)
+
+		tonConfig, ok := config.Networks["ton"]
+		if !ok {
+			cmd.PrintErr("ton network not found")
+			return
+		}
+
+		network := "mainnet"
+		if tonConfig.Testnet {
+			network = "testnet"
+		}
+
+		cmd.Printf("Scanning Ton blockchain Network: %s...\n", network)
+
+		var start, end int
+		if startFlag == 0 {
+			// todo: 2024/7/27|sean|get start block from blockchain
+			// start = tonConfig.StartHeight
+		} else {
+			start = startFlag
+		}
+
+		if endFlag == 0 {
+			// todo: 2024/7/27|sean|get current block from blockchain
+			// end = tonConfig.CurrentHeight
+		} else {
+			end = endFlag
+		}
+
+		cmd.Printf("Scanning from block %d to block %d\n", start, end)
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(scanCmd)
+	scanCmd.Flags().IntVar(&startFlag, "start", 0, "The start block number")
+	scanCmd.Flags().IntVar(&endFlag, "end", 0, "The end block number")
 }
