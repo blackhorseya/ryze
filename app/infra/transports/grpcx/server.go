840a18c799d40fe5a839facd8c10cdb530b382c9
Sean Zheng <blackhorseya@gmail.com>
2024-07-29 10:00:56 +0800

    feat: improve server logging and error handling
    - Add logging for server start and errors
    - Modify NewServer signature to include app config
    - Implement server start and serve logic
    - Add GracefulStop method for server stop
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- app/infra/transports/grpcx/server.go
+++ app/infra/transports/grpcx/server.go
@@ -1,17 +1,22 @@
 package grpcx
 
 import (
+	"net"
+
+	"github.com/blackhorseya/ryze/app/infra/configx"
 	"github.com/blackhorseya/ryze/pkg/contextx"
+	"go.uber.org/zap"
 	"google.golang.org/grpc"
 )
 
 // Server represents the grpc server.
 type Server struct {
 	grpcserver *grpc.Server
+	addr       string
 }
 
 // NewServer creates a new grpc server.
-func NewServer() (*Server, error) {
+func NewServer(app *configx.Application) (*Server, error) {
 	var server *grpc.Server
 
 	server = grpc.NewServer()
@@ -20,17 +25,32 @@ func NewServer() (*Server, error) {
 
 	return &Server{
 		grpcserver: server,
+		addr:       app.GRPC.GetAddr(),
 	}, nil
 }
 
 // Start begins the server.
 func (s *Server) Start(ctx contextx.Contextx) error {
-	// TODO: 2024/7/29|sean|implement me
-	panic("implement me")
+	go func() {
+		ctx.Info("grpc server start", zap.String("addr", s.addr))
+
+		listen, err := net.Listen("tcp", s.addr)
+		if err != nil {
+			ctx.Fatal("grpc server listen error", zap.Error(err))
+		}
+
+		err = s.grpcserver.Serve(listen)
+		if err != nil {
+			ctx.Fatal("grpc server serve error", zap.Error(err))
+		}
+	}()
+
+	return nil
 }
 
 // Stop stops the server.
 func (s *Server) Stop(ctx contextx.Contextx) error {
-	// TODO: 2024/7/29|sean|implement me
-	panic("implement me")
+	s.grpcserver.GracefulStop()
+
+	return nil
 }
