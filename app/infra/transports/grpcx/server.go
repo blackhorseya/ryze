032615058b1af30aed43d924ac4eb44dc12f3ea8
Sean Zheng <blackhorseya@gmail.com>
2024-07-29 09:52:44 +0800

    chore: refactor server initialization and introduce gRPC integration
    - Import `google.golang.org/grpc` in `server.go`
    - Initialize `grpcserver` variable in `NewServer` function
    - Update TODO comment in `NewServer` function
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- app/infra/transports/grpcx/server.go
+++ app/infra/transports/grpcx/server.go
@@ -1,11 +1,20 @@
 package grpcx
 
+import (
+	"google.golang.org/grpc"
+)
+
 // Server represents the grpc server.
 type Server struct {
+	grpcserver *grpc.Server
 }
 
 // NewServer creates a new grpc server.
 func NewServer() (*Server, error) {
-	// TODO: 2024/7/29|sean|implement grpc server
-	panic("implement me")
+	var grpcserver *grpc.Server
+	// TODO: 2024/7/29|sean|implement grpc server here
+
+	return &Server{
+		grpcserver: grpcserver,
+	}, nil
 }
