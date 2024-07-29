807fe82c38e40201ba7c740f27d3275dc73f6663
Sean Zheng <blackhorseya@gmail.com>
2024-07-29 10:30:02 +0800

    refactor: improve error handling and logging in server
    - Add logging with `go.uber.org/zap` for server start and stop
    - Remove `panic` statements and replace with appropriate error handling
    - Use `contextx.Background()` for context creation before server operations
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- adapter/block/grpc/impl.go
+++ adapter/block/grpc/impl.go
@@ -1,10 +1,14 @@
 package grpc
 
 import (
+	"fmt"
+
 	"github.com/blackhorseya/ryze/adapter/block/wirex"
 	"github.com/blackhorseya/ryze/app/infra/transports/grpcx"
 	"github.com/blackhorseya/ryze/entity/domain/block/model"
 	"github.com/blackhorseya/ryze/pkg/adapterx"
+	"github.com/blackhorseya/ryze/pkg/contextx"
+	"go.uber.org/zap"
 	"google.golang.org/grpc"
 )
 
@@ -22,13 +26,29 @@ func NewGRPC(injector *wirex.Injector, server *grpcx.Server) adapterx.Service {
 }
 
 func (i *impl) Start() error {
-	// TODO: 2024/7/29|sean|add impl logic here
-	panic("implement me")
+	ctx := contextx.Background()
+
+	err := i.server.Start(ctx)
+	if err != nil {
+		ctx.Error("Failed to start grpc server", zap.Error(err))
+		return err
+	}
+
+	ctx.Info("start grpc server")
+
+	return nil
 }
 
 func (i *impl) AwaitSignal() error {
-	// TODO: 2024/7/29|sean|add impl logic here
-	panic("implement me")
+	ctx := contextx.Background()
+	ctx.Info("receive signal to stop server")
+
+	if err := i.server.Stop(ctx); err != nil {
+		ctx.Error("Failed to stop server", zap.Error(err))
+		return fmt.Errorf("failed to stop server: %w", err)
+	}
+
+	return nil
 }
 
 // NewInitServersFn creates a new impl server init function.
