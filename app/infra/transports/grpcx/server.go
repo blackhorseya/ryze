3947da5e1cec2b09490db7f875e4a4aad33dc7b5
Sean Zheng <blackhorseya@gmail.com>
2024-07-29 09:51:13 +0800

    feat: create new `Server` type and `NewServer` function
    - Add a new `Server` type for the grpc server
    - Implement the `NewServer` function for creating a new grpc server
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- app/infra/transports/grpcx/server.go
+++ app/infra/transports/grpcx/server.go
@@ -1,1 +1,11 @@
 package grpcx
+
+// Server represents the grpc server.
+type Server struct {
+}
+
+// NewServer creates a new grpc server.
+func NewServer() (*Server, error) {
+	// TODO: 2024/7/29|sean|implement grpc server
+	panic("implement me")
+}
