1ce02ec553aee2e4116de135866afaede6a84186
Sean Zheng <blackhorseya@gmail.com>
2024-07-31 07:10:18 +0800

    feat: refactor MongoDB implementation and methods
    - Import necessary packages for MongoDB implementation
    - Create a MongoDB struct with a mongo.Client field
    - Implement GetByID, Create, Update, Delete methods with placeholder panics
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- app/domain/block/repo/block/mongodb.go
+++ app/domain/block/repo/block/mongodb.go
@@ -1,4 +1,37 @@
 package block
 
+import (
+	"github.com/blackhorseya/ryze/entity/domain/block/model"
+	"github.com/blackhorseya/ryze/entity/domain/block/repo"
+	"github.com/blackhorseya/ryze/pkg/contextx"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
 type mongodb struct {
+	rw *mongo.Client
+}
+
+// NewMongoDB is used to create an implementation of the block repository.
+func NewMongoDB(rw *mongo.Client) repo.IBlockRepo {
+	return &mongodb{rw: rw}
+}
+
+func (i *mongodb) GetByID(ctx contextx.Contextx, id string) (item *model.Block, err error) {
+	// TODO: 2024/7/31|sean|implement me
+	panic("implement me")
+}
+
+func (i *mongodb) Create(ctx contextx.Contextx, item *model.Block) (err error) {
+	// TODO: 2024/7/31|sean|implement me
+	panic("implement me")
+}
+
+func (i *mongodb) Update(ctx contextx.Contextx, item *model.Block) (err error) {
+	// TODO: 2024/7/31|sean|implement me
+	panic("implement me")
+}
+
+func (i *mongodb) Delete(ctx contextx.Contextx, id string) (err error) {
+	// TODO: 2024/7/31|sean|implement me
+	panic("implement me")
 }
