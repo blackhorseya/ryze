10a5f671a81f684c632cb2017dfb7b83c956e038
Sean Zheng <blackhorseya@gmail.com>
2024-07-31 07:08:19 +0800

    feat: refactor block repository files in domain directory
    - Add a new file `block.go` in the `entity/domain/block/repo` directory
    - Add a new file `mock_block.go` in the `entity/domain/block/repo` directory
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

---
+++ entity/domain/block/repo/block.go
@@ -0,0 +1,16 @@
+//go:generate mockgen -destination=./mock_${GOFILE} -package=${GOPACKAGE} -source=${GOFILE}
+
+package repo
+
+import (
+	"github.com/blackhorseya/ryze/entity/domain/block/model"
+	"github.com/blackhorseya/ryze/pkg/contextx"
+)
+
+// IBlockRepo is the interface for block repository.
+type IBlockRepo interface {
+	GetByID(ctx contextx.Contextx, id string) (item *model.Block, err error)
+	Create(ctx contextx.Contextx, item *model.Block) (err error)
+	Update(ctx contextx.Contextx, item *model.Block) (err error)
+	Delete(ctx contextx.Contextx, id string) (err error)
+}
