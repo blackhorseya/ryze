67ef7e931f70ed379699eaa05293d126a2de1e5c
Sean Zheng <blackhorseya@gmail.com>
2024-07-31 11:40:12 +0800

    feat: refactor MongoDB operations in block package
    - Add `go.mongodb.org/mongo-driver/mongo/options` import
    - Fix function name from `block.biz.block.mongodb.create` to `block.biz.block.mongodb.Create`
    - Add implementation for `List` function in mongodb.go
    - Set default limit to `10` if not specified
    - Set default skip to `0` if not specified
    - Sort results by `timestamp` in descending order
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- app/domain/block/repo/block/mongodb.go
+++ app/domain/block/repo/block/mongodb.go
@@ -9,6 +9,7 @@ import (
 	"github.com/blackhorseya/ryze/pkg/contextx"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.uber.org/zap"
 )
 
@@ -46,7 +47,7 @@ func (i *mongodb) GetByID(ctx contextx.Contextx, id string) (item *model.Block, 
 }
 
 func (i *mongodb) Create(ctx contextx.Contextx, item *model.Block) (err error) {
-	ctx, span := otelx.Span(ctx, "block.biz.block.mongodb.create")
+	ctx, span := otelx.Span(ctx, "block.biz.block.mongodb.Create")
 	defer span.End()
 
 	timeout, cancelFunc := contextx.WithTimeout(ctx, defaultTimeout)
@@ -66,6 +67,47 @@ func (i *mongodb) List(
 	ctx contextx.Contextx,
 	condition repo.ListCondition,
 ) (items []*model.Block, total int, err error) {
-	// TODO: 2024/7/31|sean|implement me
-	panic("implement me")
+	ctx, span := otelx.Span(ctx, "block.biz.block.mongodb.List")
+	defer span.End()
+
+	timeout, cancelFunc := contextx.WithTimeout(ctx, defaultTimeout)
+	defer cancelFunc()
+
+	filter := bson.M{}
+
+	limit := condition.Limit
+	if limit == 0 {
+		limit = 10
+	}
+	skip := condition.Skip
+	if skip < 0 {
+		skip = 0
+	}
+	opts := options.Find().SetSort(bson.M{"timestamp": -1}).SetLimit(int64(limit)).SetSkip(int64(skip))
+
+	cur, err := i.rw.Database(dbName).Collection(collName).Find(timeout, filter, opts)
+	if err != nil {
+		ctx.Error("failed to find blocks from mongodb", zap.Error(err))
+		return nil, 0, err
+	}
+	defer cur.Close(timeout)
+
+	for cur.Next(timeout) {
+		var got blockDocument
+		err = cur.Decode(&got)
+		if err != nil {
+			ctx.Error("failed to decode a block from mongodb", zap.Error(err))
+			return nil, 0, err
+		}
+
+		items = append(items, got.Metadata)
+	}
+
+	count, err := i.rw.Database(dbName).Collection(collName).CountDocuments(timeout, filter)
+	if err != nil {
+		ctx.Error("failed to count blocks from mongodb", zap.Error(err))
+		return nil, 0, err
+	}
+
+	return items, int(count), nil
 }
