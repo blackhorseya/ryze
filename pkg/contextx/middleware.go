7715e027d147f9f0c56aa2372e27c0359d10f2a7
Sean Zheng <blackhorseya@gmail.com>
2024-08-11 09:27:41 +0800

    refactor: refactor server middleware and implement new functions
    - Update import paths for middleware packages in `server.go`
    - Refactor `NewServer` function in `server.go` to use updated middleware package names
    - Implement `AddContextxMiddleware`, `UnaryServerInterceptor`, and `StreamServerInterceptor` functions in `middleware.go`
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- pkg/contextx/middleware.go
+++ pkg/contextx/middleware.go
@@ -1,7 +1,10 @@
 package contextx
 
 import (
+	"context"
+
 	"github.com/gin-gonic/gin"
+	grpc_middleware "github.com/grpc-ecosystem/go-grpc-middleware"
 	"google.golang.org/grpc"
 )
 
@@ -16,12 +19,20 @@ func AddContextxMiddleware() gin.HandlerFunc {
 
 // UnaryServerInterceptor is used to create a new unary interceptor
 func UnaryServerInterceptor() grpc.UnaryServerInterceptor {
-	// TODO: 2024/8/11|sean|implement me
-	panic("implement me")
+	return func(c context.Context, req any, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp any, err error) {
+		ctx := WithContext(c)
+
+		return handler(ctx, req)
+	}
 }
 
 // StreamServerInterceptor is used to create a new stream interceptor
 func StreamServerInterceptor() grpc.StreamServerInterceptor {
-	// TODO: 2024/8/11|sean|implement me
-	panic("implement me")
+	return func(srv any, ss grpc.ServerStream, info *grpc.StreamServerInfo, handler grpc.StreamHandler) error {
+		ctx := WithContext(ss.Context())
+		wrappedStream := grpc_middleware.WrapServerStream(ss)
+		wrappedStream.WrappedContext = ctx
+
+		return handler(srv, ss)
+	}
 }
