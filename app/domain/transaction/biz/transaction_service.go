d911224b7819d9d10287fac0c69a572b00488600
Sean Zheng <blackhorseya@gmail.com>
2024-08-12 14:11:31 +0800

    feat: improve transaction handling and imports
    - Add `github.com/blackhorseya/ryze/pkg/contextx` and `go.uber.org/zap` imports
    - Implement context handling for transaction listing
    - Send transactions through the stream in the `ListTransactions` function
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- app/domain/transaction/biz/transaction_service.go
+++ app/domain/transaction/biz/transaction_service.go
@@ -3,9 +3,12 @@ package biz
 import (
 	"context"
 
+	"github.com/blackhorseya/ryze/app/infra/otelx"
 	"github.com/blackhorseya/ryze/app/infra/tonx"
 	txB "github.com/blackhorseya/ryze/entity/domain/transaction/biz"
 	txM "github.com/blackhorseya/ryze/entity/domain/transaction/model"
+	"github.com/blackhorseya/ryze/pkg/contextx"
+	"go.uber.org/zap"
 )
 
 type txService struct {
@@ -28,6 +31,24 @@ func (i *txService) ListTransactions(
 	req *txB.ListTransactionsRequest,
 	stream txB.TransactionService_ListTransactionsServer,
 ) error {
+	ctx, err := contextx.FromContext(stream.Context())
+	if err != nil {
+		return err
+	}
+
+	ctx, span := otelx.Span(ctx, "transaction.biz.ListTransactions")
+	defer span.End()
+
+	var txList []*txM.Transaction
+
 	// TODO: 2024/8/12|sean|implement me
-	panic("implement me")
+
+	for _, tx := range txList {
+		if err = stream.Send(tx); err != nil {
+			ctx.Error("send transaction error", zap.Error(err))
+			return err
+		}
+	}
+
+	return nil
 }
