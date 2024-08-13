69faac374f9b9a609d59e8fc1efaad7ad1ab0a2d
Sean Zheng <blackhorseya@gmail.com>
2024-08-13 08:56:35 +0800

    refactor: enhance NewTransactionFromTon function and fields
    - Fill in more fields in the NewTransactionFromTon function
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- entity/domain/transaction/model/transaction.go
+++ entity/domain/transaction/model/transaction.go
@@ -6,7 +6,7 @@ import (
 
 // NewTransactionFromTon is used to create a new transaction from ton
 func NewTransactionFromTon(value *tlb.Transaction) *Transaction {
-	// TODO: 2024/8/12|sean|implement me
+	// TODO: 2024/8/12|sean|fill more fields
 	return &Transaction{
 		Id:        value.Hash,
 		BlockId:   nil,
