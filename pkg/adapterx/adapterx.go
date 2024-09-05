a2f44783b83f3a6d47c0f1c4a4f799e099794609
Sean Zheng <blackhorseya@gmail.com>
2024-09-05 14:39:35 +0900

    feat: refactor interfaces and imports
    - Add `context` import in `adapterx.go`
    - Update comment deprecating `Service` interface
    - Add `Server` interface with `Start` and `Shutdown` methods
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- pkg/adapterx/adapterx.go
+++ pkg/adapterx/adapterx.go
@@ -1,17 +1,26 @@
 package adapterx
 
 import (
+	"context"
+
 	"github.com/blackhorseya/ryze/pkg/contextx"
 	"github.com/gin-gonic/gin"
 )
 
 // Service is the interface that wraps the basic Serve method.
+// Deprecated: use Server instead.
 type Service interface {
 	// Start a service asynchronously.
 	Start(ctx contextx.Contextx) error
 
 	// AwaitSignal waits for a signal to shut down the service.
 	AwaitSignal(ctx contextx.Contextx) error
+}
+
+// Server is the interface that wraps the basic Serve method.
+type Server interface {
+	Start(c context.Context) error
+	Shutdown(c context.Context) error
 }
 
 // Restful is the interface that wraps the restful api method.
