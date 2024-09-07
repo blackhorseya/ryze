078ceff0bbca0b6598a52a741f7d14beef534dd6
Sean Zheng <blackhorseya@gmail.com>
2024-09-07 12:51:30 +0900

    chore: refactor external block service implementation
    - Delete `app/domain/block/block_service_external_test.go`
    - Remove `NewExternalBlockService` implementation from `app/domain/block/wire.go`
    - Delete `app/domain/block/wire_gen.go`
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- app/domain/block/wire.go
+++ app/domain/block/wire.go
@@ -1,15 +1,8 @@
-//go:build wireinject
-
-//go:generate wire
-
 package block
 
 import (
 	"github.com/blackhorseya/ryze/app/infra/storage/mongodbx"
-	"github.com/blackhorseya/ryze/app/infra/tonx"
-	"github.com/blackhorseya/ryze/entity/domain/block/biz"
 	"github.com/google/wire"
-	"go.mongodb.org/mongo-driver/mongo"
 )
 
 // ProviderSet is used to provide a new model.BlockServiceServer
@@ -17,7 +10,3 @@ var ProviderSet = wire.NewSet(
 	NewBlockService,
 	mongodbx.NewBlockRepo,
 )
-
-func NewExternalBlockService(client *tonx.Client, rw *mongo.Client) biz.BlockServiceServer {
-	panic(wire.Build(ProviderSet))
-}
