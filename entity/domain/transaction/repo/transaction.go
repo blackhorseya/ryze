5102bf0540a4104d041989948b13c16fc9c37188
Sean Zheng <blackhorseya@gmail.com>
2024-09-13 12:04:30 +0800

    feat: refactor transaction handling with new interfaces
    - Added a new file `mock_transaction.go` with code generated by MockGen
    - Added a new file `transaction.go` with the definition of `ITransactionRepo` interface
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

---
+++ entity/domain/transaction/repo/transaction.go
@@ -0,0 +1,24 @@
+//go:generate mockgen -destination=./mock_${GOFILE} -package=${GOPACKAGE} -source=${GOFILE}
+
+package repo
+
+import (
+	"context"
+
+	"github.com/blackhorseya/ryze/entity/domain/transaction/model"
+)
+
+// ListTransactionsCondition is the condition for list.
+type ListTransactionsCondition struct {
+	Limit  int
+	Offset int
+}
+
+// ITransactionRepo is the interface for transaction repository.
+type ITransactionRepo interface {
+	Create(c context.Context, item *model.Transaction) (err error)
+	GetByID(c context.Context, id string) (item *model.Transaction, err error)
+	List(c context.Context, cond ListTransactionsCondition) (items []*model.Transaction, total int, err error)
+	Update(c context.Context, item *model.Transaction) (err error)
+	Delete(c context.Context, id string) (err error)
+}
