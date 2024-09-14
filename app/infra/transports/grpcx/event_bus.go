1c5e2bcb8c79af0aaae2a0253a154ee0c25c7ff7
Sean Zheng <blackhorseya@gmail.com>
2024-09-14 11:47:29 +0800

    feat: refactor event handling with new EventBus structure
    - Add a new struct `eventBus` in the `event_bus.go` file
    - Implement the `NewEventBus` function to return a pointer to `eventBus`
    - Implement the `Subscribe` method for `eventBus`
    - Implement the `Publish` method for `eventBus`
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- app/infra/transports/grpcx/event_bus.go
+++ app/infra/transports/grpcx/event_bus.go
@@ -4,8 +4,20 @@ import (
 	"github.com/blackhorseya/ryze/pkg/eventx"
 )
 
+type eventBus struct {
+}
+
 // NewEventBus creates a new gRPC event bus.
 func NewEventBus() eventx.EventBus {
-	// TODO: 2024/9/14|sean|implement this
-	panic("not implemented")
+	return &eventBus{}
+}
+
+func (bus *eventBus) Subscribe(handler eventx.EventHandler) error {
+	// TODO: 2024/9/14|sean|implement me
+	panic("implement me")
+}
+
+func (bus *eventBus) Publish(event eventx.DomainEvent) error {
+	// TODO: 2024/9/14|sean|implement me
+	panic("implement me")
 }
