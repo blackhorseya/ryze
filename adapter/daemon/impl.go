fb064cf6dedb7d918d0cd764aa424ee4dfe87878
Sean Zheng <blackhorseya@gmail.com>
2024-09-14 10:09:41 +0800

    feat: consolidate dependency injection logic in daemon
    - Add a new file `impl.go` in the `daemon` directory
    - Add a new file `injector.go` in the `daemon` directory
    - Add a new file `main_test.go` in the `daemon` directory
    - Add a new file `wire.go` in the `daemon` directory
    - Add a new file `wire_gen.go` in the `daemon` directory
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

---
+++ adapter/daemon/impl.go
@@ -0,0 +1,10 @@
+package daemon
+
+import (
+	"github.com/blackhorseya/ryze/pkg/adapterx"
+)
+
+// NewServer is a function to create a new server.
+func NewServer() (adapterx.Server, func(), error) {
+	panic("not implemented")
+}
