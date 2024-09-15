0d9214fd6c64318302b915f9ff690dfce4ebb92f
Sean Zheng <blackhorseya@gmail.com>
2024-09-15 10:44:35 +0800

    refactor: refactor client-side code for improved error handling
    - Update the dial method to use "daemon" instead of "block-scanner" in block_grpc_client.go
    - Change the error message to include more specific details in block_grpc_client.go
    - Add the fmt package import in transaction_grpc_client.go
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- app/domain/block/block_grpc_client.go
+++ app/domain/block/block_grpc_client.go
@@ -9,9 +9,9 @@ import (
 
 // NewBlockServiceClient is used to create a new block service tonClient
 func NewBlockServiceClient(client *grpcx.Client) (biz.BlockServiceClient, error) {
-	conn, err := client.Dial("block-scanner")
+	conn, err := client.Dial("daemon")
 	if err != nil {
-		return nil, fmt.Errorf("failed to dial `block-grpc` error: %w", err)
+		return nil, fmt.Errorf("failed to dial `daemon` error: %w", err)
 	}
 
 	return biz.NewBlockServiceClient(conn), nil
--- app/domain/transaction/transaction_grpc_client.go
+++ app/domain/transaction/transaction_grpc_client.go
@@ -1,15 +1,17 @@
 package transaction
 
 import (
+	"fmt"
+
 	"github.com/blackhorseya/ryze/app/infra/transports/grpcx"
 	"github.com/blackhorseya/ryze/entity/domain/transaction/biz"
 )
 
 // NewTransactionServiceClient is used to create a new transaction service client.
 func NewTransactionServiceClient(client *grpcx.Client) (biz.TransactionServiceClient, error) {
-	conn, err := client.Dial("block-scanner")
+	conn, err := client.Dial("daemon")
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("failed to dial `daemon` error: %w", err)
 	}
 
 	return biz.NewTransactionServiceClient(conn), nil
