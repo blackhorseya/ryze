635f2824732a15c35e25b98bbb1d6a333541562f
Sean Zheng <blackhorseya@gmail.com>
2024-10-04 11:36:04 +0800

    refactor: eliminate Restful interface from adapterx package
    - Remove the Restful interface and its associated methods from the adapterx package
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- pkg/adapterx/adapterx.go
+++ pkg/adapterx/adapterx.go
@@ -2,23 +2,10 @@ package adapterx
 
 import (
 	"context"
-
-	"github.com/gin-gonic/gin"
 )
 
 // Server is the interface that wraps the basic Serve method.
 type Server interface {
 	Start(c context.Context) error
 	Shutdown(c context.Context) error
-}
-
-// Restful is the interface that wraps the restful api method.
-type Restful interface {
-	Server
-
-	// InitRouting init the routing of restful api.
-	InitRouting() error
-
-	// GetRouter returns the router of restful api.
-	GetRouter() *gin.Engine
 }
