64f6608019d092d6287d1e511e9dc99ef4d46d6a
Sean Zheng <blackhorseya@gmail.com>
2024-10-04 10:49:26 +0800

    feat: enhance gRPC call reliability with timeout and retries
    - Add timeout and retry options for gRPC calls
    - Set maximum retries to 3 for gRPC requests
    - Introduce a per-retry timeout of 2 seconds
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- app/infra/transports/grpcx/client.go
+++ app/infra/transports/grpcx/client.go
@@ -3,9 +3,11 @@ package grpcx
 import (
 	"errors"
 	"fmt"
+	"time"
 
 	"github.com/blackhorseya/ryze/app/infra/configx"
 	grpc_middleware "github.com/grpc-ecosystem/go-grpc-middleware"
+	grpc_retry "github.com/grpc-ecosystem/go-grpc-middleware/retry"
 	grpc_prometheus "github.com/grpc-ecosystem/go-grpc-prometheus"
 	"go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc"
 	"google.golang.org/grpc"
@@ -35,15 +37,23 @@ func (c *Client) Dial(service string) (*grpc.ClientConn, error) {
 		return nil, errors.New("grpc url or port is empty")
 	}
 
+	// Retry options
+	retryOptions := []grpc_retry.CallOption{
+		grpc_retry.WithMax(3),                           // Retry up to 3 times
+		grpc_retry.WithPerRetryTimeout(2 * time.Second), // Timeout for each retry
+	}
+
 	target := fmt.Sprintf("%s:%d", app.GRPC.URL, app.GRPC.Port)
 	options := []grpc.DialOption{
 		grpc.WithTransportCredentials(insecure.NewCredentials()),
 		grpc.WithStatsHandler(otelgrpc.NewClientHandler()),
 		grpc.WithUnaryInterceptor(grpc_middleware.ChainUnaryClient(
 			grpc_prometheus.UnaryClientInterceptor,
+			grpc_retry.UnaryClientInterceptor(retryOptions...),
 		)),
 		grpc.WithStreamInterceptor(grpc_middleware.ChainStreamClient(
 			grpc_prometheus.StreamClientInterceptor,
+			grpc_retry.StreamClientInterceptor(retryOptions...),
 		)),
 	}
 
