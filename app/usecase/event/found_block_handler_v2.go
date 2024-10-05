16ec07cc0a13db67ced7a3966a87e837e599c07b
Sean Zheng <blackhorseya@gmail.com>
2024-10-05 12:11:50 +0800

    refactor: improve block event handling and logging
    - Simplify error handling by removing redundant checks for context cancellation
    - Add a new import for the block model in the found block handler
    - Enhance event handling by adding type assertion for FoundBlockEvent
    - Improve logging when handling new block events
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- app/usecase/event/found_block_handler_v2.go
+++ app/usecase/event/found_block_handler_v2.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	blockB "github.com/blackhorseya/ryze/entity/domain/block/biz"
+	"github.com/blackhorseya/ryze/entity/domain/block/model"
 	txB "github.com/blackhorseya/ryze/entity/domain/transaction/biz"
 	"github.com/blackhorseya/ryze/pkg/contextx"
 	"github.com/blackhorseya/ryze/pkg/eventx"
@@ -27,9 +28,50 @@ func NewFoundBlockHandlerV2(
 }
 
 func (i *foundBlockHandlerV2) Handle(event eventx.DomainEvent) {
-	// TODO: 2024/10/5|sean|implement me
 	ctx, span := contextx.StartSpan(context.Background(), "event.found_block_handler_v2.Handle")
 	defer span.End()
 
-	ctx.Info("found block event", zap.Any("event", &event))
+	blockEvent, ok := event.(*model.FoundBlockEvent)
+	if !ok {
+		ctx.Error("failed to cast event to FoundBlockEvent", zap.Any("event", event))
+		return
+	}
+
+	ctx.Info("start handle new block event", zap.String("block_id", blockEvent.Block.Id))
+
+	// handle new block by id
+	newBlock, err := i.blockClient.FoundNewBlockNonStream(ctx, &blockB.FoundNewBlockRequest{
+		Workchain: blockEvent.Block.Workchain,
+		Shard:     blockEvent.Block.Shard,
+		SeqNo:     blockEvent.Block.SeqNo,
+	})
+	if err != nil {
+		ctx.Error("failed to handle new block", zap.Error(err))
+		return
+	}
+	ctx.Debug(
+		"found new block",
+		zap.String("block_id", newBlock.Id),
+		zap.Time("block_time", newBlock.Timestamp.AsTime()),
+	)
+
+	// TODO: 2024/10/5|sean|handle new block transactions
+	// handle new block transactions
+	// stream, err := i.txClient.ProcessBlockTransactionsNonStream(ctx, newBlock)
+	// if err != nil {
+	// 	ctx.Error("failed to handle new block transactions", zap.Error(err))
+	// 	return
+	// }
+	//
+	// for {
+	// 	tx, err2 := stream.Recv()
+	// 	if errors.Is(err2, io.EOF) {
+	// 		break
+	// 	}
+	// 	if err2 != nil {
+	// 		ctx.Error("failed to receive new block transaction", zap.Error(err2))
+	// 		break
+	// 	}
+	// 	ctx.Debug("found new block transaction", zap.String("tx_id", string(tx.Id)))
+	// }
 }
