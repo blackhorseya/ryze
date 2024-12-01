2ab190665f0b01f444f16f249d6a7fffb7f072bd
Sean Zheng <blackhorseya@gmail.com>
2024-12-01 09:41:21 +0800

    feat: enhance block monitoring and error handling in adapter
    - Improve error handling in the `getNotSeenShards` function by restructuring the error message format.
    - Add a `nolint` directive to suppress security warnings in the block ID formatting.
    - Implement a master chain info retrieval in the block adapter, including error logging for failures.
    - Introduce a mechanism to continuously monitor new blocks across all shards in the block adapter.
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- entity/domain/block/model/block.go
+++ entity/domain/block/model/block.go
@@ -10,7 +10,7 @@ import (
 // NewBlock is used to create a new block.
 func NewBlock(workchain int32, shard int64, seqno uint32) (*Block, error) {
 	return &Block{
-		Id:        fmt.Sprintf("%d:%x:%d", workchain, uint64(shard), seqno),
+		Id:        fmt.Sprintf("%d:%x:%d", workchain, uint64(shard), seqno), //nolint:gosec // ignore gosec
 		Workchain: workchain,
 		Shard:     shard,
 		SeqNo:     seqno,
--- internal/app/infra/datasource/ton/block_adapter_impl.go
+++ internal/app/infra/datasource/ton/block_adapter_impl.go
@@ -2,15 +2,21 @@ package ton
 
 import (
 	"context"
+	"sync"
 
 	blockB "github.com/blackhorseya/ryze/entity/domain/block/biz"
 	"github.com/blackhorseya/ryze/internal/app/repo"
 	"github.com/blackhorseya/ryze/internal/shared/tonx"
+	"github.com/blackhorseya/ryze/pkg/contextx"
+	"github.com/xssnick/tonutils-go/ton"
+	"go.uber.org/zap"
 )
 
 // BlockAdapterImpl is the implementation for block adapter.
 type BlockAdapterImpl struct {
 	client *tonx.Client
+
+	shardLastSeqno sync.Map
 }
 
 // NewBlockAdapterImpl is used to create a new block adapter implementation.
@@ -30,6 +36,50 @@ func (i *BlockAdapterImpl) ScanBlock(
 	req repo.ScanBlockRequest,
 	blockCh chan<- *blockB.Block,
 ) error {
-	// TODO: 2024/12/1|sean|implement me
-	panic("implement me")
+	ctx, span := contextx.StartSpan(c, "datasource.ton.BlockAdapterImpl.ScanBlock")
+	defer span.End()
+
+	api := ton.NewAPIClient(i.client, ton.ProofCheckPolicyFast).WithRetry()
+	api.SetTrustedBlockFromConfig(i.client.Config)
+
+	// 獲取主鏈資訊
+	master, err := api.GetMasterchainInfo(ctx)
+	if err != nil {
+		ctx.Error("failed to get master-chain info", zap.Error(err))
+		return err
+	}
+	ctx.Info("master proofs chain successfully verified, all data is now safe and trusted!")
+
+	// 綁定單一伺服器的上下文以保持一致性
+	stickyContext := api.Client().StickyContext(ctx)
+
+	// 從主鏈獲取所有的分片資訊
+	firstShards, err := api.GetBlockShardsInfo(stickyContext, master)
+	if err != nil {
+		ctx.Error("failed to get block shards info", zap.Error(err), zap.Any("master", &master))
+		return err
+	}
+
+	// 初始化分片序號的記錄
+	for _, shard := range firstShards {
+		i.shardLastSeqno.Store(tonx.GetShardID(shard), shard.SeqNo)
+	}
+
+	// 持續監聽所有分片上的新區塊
+	for {
+		// 獲取每個 workchain 和 shard 上的新區塊
+		currentShards, err2 := api.GetBlockShardsInfo(ctx, master)
+		if err2 != nil {
+			ctx.Error("failed to get block shards info", zap.Error(err2), zap.Any("master", &master))
+			return err2
+		}
+
+		for _, shard := range currentShards {
+			// 檢查是否有新的區塊
+			value, ok := i.shardLastSeqno.Load(tonx.GetShardID(shard))
+			if ok && shard.SeqNo <= value.(uint32) {
+				continue
+			}
+		}
+	}
 }
