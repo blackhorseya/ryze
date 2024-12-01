6c78307e818f8c1f6e5b7537d61d72e68c2c2b2a
Sean Zheng <blackhorseya@gmail.com>
2024-12-01 09:24:27 +0800

    feat: introduce block operation interfaces and mocks
    - Add a new struct `ListBlockCondition` with fields for time range and pagination.
    - Introduce new interfaces: `BlockCreator`, `BlockGetter`, and `BlockRepo` for block operations.
    - Create a new file `mock_block.go` for mocking the interfaces related to block operations.
    - Implement mock types for `BlockCreator`, `BlockGetter`, and `BlockRepo` with methods for testing purposes.
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- internal/app/repo/block.go
+++ internal/app/repo/block.go
@@ -4,8 +4,49 @@ package repo
 
 import (
 	"context"
+	"time"
 
+	"github.com/blackhorseya/ryze/entity/domain/block/biz"
 	"github.com/blackhorseya/ryze/entity/domain/block/model"
+)
+
+// ListBlockCondition is the condition for list block.
+type ListBlockCondition struct {
+	Start  time.Time
+	End    time.Time
+	Limit  int64
+	Offset int64
+}
+
+type (
+	// BlockCreator is the creator for block.
+	BlockCreator interface {
+		Create(c context.Context, block *biz.Block) error
+	}
+
+	// BlockGetter is the getter for block.
+	BlockGetter interface {
+		GetByID(c context.Context, id string) (*biz.Block, error)
+		List(c context.Context, cond ListBlockCondition) ([]*biz.Block, int, error)
+	}
+
+	// BlockRepo is the repository for block.
+	BlockRepo interface {
+		BlockCreator
+		BlockGetter
+	}
+)
+
+// ScanBlockRequest is the request for scan block.
+type ScanBlockRequest struct {
+	StartHeight uint32
+	EndHeight   uint32
+}
+
+type (
+	BlockAdapter interface {
+		ScanBlock(c context.Context, req ScanBlockRequest, blockCh chan<- *biz.Block) error
+	}
 )
 
 // ListCondition is the condition for list.
