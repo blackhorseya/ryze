f2ce390f3015b4bbd44913201149dd3077628fcc
Sean Zheng <blackhorseya@gmail.com>
2024-12-01 09:29:35 +0800

    refactor: refactor BlockAdapter for consistency and client support
    - Rename the file from block_adapter_impl.go under tonx to block_adapter_impl.go under ton
    - Change the package name from tonx to ton
    - Add a client field to the BlockAdapterImpl struct
    - Modify the NewBlockAdapterImpl function to accept a client parameter and initialize it
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- internal/app/infra/datasource/ton/block_adapter_impl.go
+++ internal/app/infra/datasource/ton/block_adapter_impl.go
@@ -1,19 +1,23 @@
-package tonx
+package ton
 
 import (
 	"context"
 
 	blockB "github.com/blackhorseya/ryze/entity/domain/block/biz"
 	"github.com/blackhorseya/ryze/internal/app/repo"
+	"github.com/blackhorseya/ryze/internal/shared/tonx"
 )
 
 // BlockAdapterImpl is the implementation for block adapter.
 type BlockAdapterImpl struct {
+	client *tonx.Client
 }
 
 // NewBlockAdapterImpl is used to create a new block adapter implementation.
-func NewBlockAdapterImpl() *BlockAdapterImpl {
-	return &BlockAdapterImpl{}
+func NewBlockAdapterImpl(client *tonx.Client) *BlockAdapterImpl {
+	return &BlockAdapterImpl{
+		client: client,
+	}
 }
 
 // NewBlockAdapter is used to create a new block adapter.
