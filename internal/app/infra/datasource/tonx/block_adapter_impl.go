1a8073f4bd8d70e96073a6702a90f86ffc03011f
Sean Zheng <blackhorseya@gmail.com>
2024-12-01 09:26:27 +0800

    feat: implement BlockAdapter for improved functionality
    - Introduce a new file for the block adapter implementation
    - Define the BlockAdapterImpl type
    - Implement a constructor function for BlockAdapterImpl
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

---
+++ internal/app/infra/datasource/tonx/block_adapter_impl.go
@@ -0,0 +1,10 @@
+package tonx
+
+// BlockAdapterImpl is the implementation for block adapter.
+type BlockAdapterImpl struct {
+}
+
+// NewBlockAdapterImpl is used to create a new block adapter implementation.
+func NewBlockAdapterImpl() *BlockAdapterImpl {
+	return &BlockAdapterImpl{}
+}
