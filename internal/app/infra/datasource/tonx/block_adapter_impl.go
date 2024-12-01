9aeeb0ac3a7bd5c144ae4cc6a9384ff3063d0533
Sean Zheng <blackhorseya@gmail.com>
2024-12-01 09:27:23 +0800

    feat: enhance block adapter functionality and error handling
    - Add import statements for context, blockB, and repo packages
    - Introduce a new function NewBlockAdapter to create a new block adapter
    - Expand the ScanBlock method with additional parameters and a panic statement for unimplemented functionality
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- internal/app/infra/datasource/tonx/block_adapter_impl.go
+++ internal/app/infra/datasource/tonx/block_adapter_impl.go
@@ -1,5 +1,12 @@
 package tonx
 
+import (
+	"context"
+
+	blockB "github.com/blackhorseya/ryze/entity/domain/block/biz"
+	"github.com/blackhorseya/ryze/internal/app/repo"
+)
+
 // BlockAdapterImpl is the implementation for block adapter.
 type BlockAdapterImpl struct {
 }
@@ -7,4 +14,18 @@ type BlockAdapterImpl struct {
 // NewBlockAdapterImpl is used to create a new block adapter implementation.
 func NewBlockAdapterImpl() *BlockAdapterImpl {
 	return &BlockAdapterImpl{}
+}
+
+// NewBlockAdapter is used to create a new block adapter.
+func NewBlockAdapter(impl *BlockAdapterImpl) repo.BlockAdapter {
+	return impl
+}
+
+func (i *BlockAdapterImpl) ScanBlock(
+	c context.Context,
+	req repo.ScanBlockRequest,
+	blockCh chan<- *blockB.Block,
+) error {
+	// TODO: 2024/12/1|sean|implement me
+	panic("implement me")
 }
